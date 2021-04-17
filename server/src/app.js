fe71797a6cdfef46cd7e0af6b1522e90b4c04030
Souvik De <souvikde.ns@gmail.com>
2021-04-18 03:58:39 +0530

    cleanup

--- server/src/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const app = express();
-
-
-
-module.exports = app;
--- server/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on port ${process.env.PORT || 3000}`);
-})
