4d757934d37251da9c8a23c41ce359114d701eed
Souvik De <souvikde.ns@gmail.com>
2021-04-18 14:01:47 +0530

    added prop-types

--- ui.js
+++ ui.js
@@ -1,11 +1,16 @@
 'use strict';
 const React = require('react');
 const {Text} = require('ink');
+const PropTypes = require('prop-types')
 
 const App = ({name = 'Stranger'}) => (
 	<Text>
 		Hello, <Text color="green">{name}</Text>
 	</Text>
 );
+
+App.propTypes = {
+	name: PropTypes.string
+}
 
 module.exports = App;
