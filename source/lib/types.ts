fdada7745f55ec1a10d87fe98594ed751d7868b3
Souvik De <souvikde.ns@gmail.com>
2021-04-20 00:30:40 +0530

    Create types.ts

---
+++ source/lib/types.ts
@@ -0,0 +1,20 @@
+export enum PokemonType {
+    normal,
+    fire,
+    water,
+    grass,
+    electric,
+    ice,
+    fighting,
+    poison,
+    ground,
+    flying,
+    psychic,
+    bug,
+    rock,
+    ghost,
+    dark,
+    dragon,
+    steel,
+    fairy
+}
