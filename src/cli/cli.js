578e04d110e953bfcc3ca668e32aa42e18230e93
Restyled.io <commits@restyled.io>
2021-04-19 10:53:21 +0000

    Restyled by clang-format

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,48 +1,30 @@
 #!/usr/bin/env node
 const React = require("react");
 const importJsx = require("import-jsx");
-const { render } = require("ink");
+const {render} = require("ink");
 const meow = require("meow");
 const meowHelp = require('cli-meow-help');
-const { viewFileFinder } = require("./util");
+const {viewFileFinder} = require("./util");
 const pkg = require('../../package.json');
 
 const ui = importJsx(viewFileFinder("ui.js"));
 
-const cli = meow(meowHelp({
-	name: "poki",
-	commands: {
-		pokedex: {
-			desc: "get data of all the pokemons in the game"
-		},
-		catch: {
-			desc: "catch pokemon"
-		},
-		gym: {
-			desc: "battle gyms to win badges"
-		},
-		league: {
-			desc: "enter in leagues"
-		},
-		init: {
-			desc: "create your account"
-		},
-		whoami: {
-			desc: "get Profile data"
-		}
-	},
-	flags: {
-		help: {
-			desc: "get help for commands",
-			type: "boolean"
-		},
-		version: {
-			desc: "get version",
-			type: "boolean"
-		}
-	}
-}), {
-	version: pkg.version
-});
+const cli =
+    meow(meowHelp({
+           name : "poki",
+           commands : {
+             pokedex : {desc : "get data of all the pokemons in the game"},
+             catch : {desc : "catch pokemon"},
+             gym : {desc : "battle gyms to win badges"},
+             league : {desc : "enter in leagues"},
+             init : {desc : "create your account"},
+             whoami : {desc : "get Profile data"}
+           },
+           flags : {
+             help : {desc : "get help for commands", type : "boolean"},
+             version : {desc : "get version", type : "boolean"}
+           }
+         }),
+         {version : pkg.version});
 
 render(React.createElement(ui, cli.flags));
