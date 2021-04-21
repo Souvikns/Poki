7406a109ff51aea9239a3ca544b22d855a3ffee2
Souvik De <souvikde.ns@gmail.com>
2021-04-22 03:22:42 +0530

    fixing pokedex component

--- source/lib/data/pokemon.ts
+++ source/lib/data/pokemon.ts
@@ -1,8 +1,12 @@
-import {Pokemon, PokemonName, Type} from '../types';
+import { Pokemon, PokemonName, Type } from '../types';
 
 export const PokemonData: Array<Pokemon> = [
     {
         name: PokemonName.bulbasaur,
+        type: [Type.grass, Type.poison]
+    },
+    {
+        name: PokemonName.ivysaur,
         type: [Type.grass, Type.poison]
     }
 ]
