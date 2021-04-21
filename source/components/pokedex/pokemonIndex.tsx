72cadb4f6c73b688b903de00ad201db0a0deff59
Souvik De <souvikde.ns@gmail.com>
2021-04-21 22:47:59 +0530

    Update pokemonIndex.tsx

--- source/components/pokedex/pokemonIndex.tsx
+++ source/components/pokedex/pokemonIndex.tsx
@@ -1,10 +1,52 @@
-import React, {FC} from 'react';
-import {Text} from 'ink';
-import {PokemonName} from '../../lib'
+import React, { FC, useState, useEffect } from 'react';
+import { Text, useInput, render } from 'ink';
+import { PokemonName } from '../../lib'
+import SelectInput from 'ink-select-input';
+import _ from 'lodash';
+import Pokemon from './pokemon';
 
-const PokemonIndex: FC<{}> = ({}) => {
+const PokemonIndex: FC<{}> = ({ }) => {
+
+    const [search] = useState("")
+    const [pokemons] = useState(Object.values(PokemonName));
+    const [items, setItems] = useState([]);
+    const [pos, setPos] = useState(0);
+
+    useEffect(() => {
+        let arr: any = _.chunk(pokemons, 10)[pos]?.map(pk => ({ label: pk, value: pk }))
+        setItems(arr);
+    }, [])
+
+    useInput((input, key) => {
+        let { escape, rightArrow, leftArrow } = key
+        if (escape) {
+            process.exit();
+        }
+        if (rightArrow) {
+            setPos(pos + 1);
+            let as: any = _.chunk(pokemons, 10)[pos]?.map(pk => ({ label: pk, value: pk }))
+            setItems(as)
+        }
+
+        if (leftArrow) {
+            setPos(pos - 1);
+            let ad: any = _.chunk(pokemons, 10)[pos]?.map(pk => ({ label: pk, value: pk }))
+            setItems(ad)
+        }
+
+        if (input) {
+            
+        }
+    })
+
+    const handleSubmit = (item: any) => {
+        render(<Pokemon name={item.value} />)
+    }
+
     return <>
-    {Object.values(PokemonName).map(key => <Text key={key}>{key}</Text>)}
+        <Text>{search}</Text>
+
+        <SelectInput items={items} onSelect={handleSubmit} />
     </>
 }
 
