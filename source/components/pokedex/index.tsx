e4feb9728f8a4ecbcda68d0c481a6b329eb76bed
Souvik De <souvikde.ns@gmail.com>
2021-04-25 14:02:42 +0530

    created pokemon selector pokedex view

--- source/components/pokedex/index.tsx
+++ source/components/pokedex/index.tsx
@@ -1,18 +1,58 @@
-import React, { FC } from 'react';
-import { Text } from 'ink'
+import React, { FC, useState } from 'react';
+import { useInput, render } from 'ink'
 import { Database } from '../../lib'
+import _ from 'lodash';
+import SelectInput from 'ink-select-input';
 
 // importing components
 import PokemonUI from './pokemon';
 
 const Pokedex: FC<{ flag: any }> = ({ flag }) => {
 
+    let [pos, setPos] = useState(0);
+    const pokemonsList = _.chunk(Database.getAllPokemon(), 10);
+    let [items, setItems] = useState(pokemonsList[pos]?.map(pokemon => ({ label: pokemon.name, value: pokemon.id })));
+
+    const selectHandler = (item: any) => {
+        render(<PokemonUI name={item.label} />)
+    }
+
+    type Shift = "left" | "right"
+
+    const listShift = (shift: Shift) => {
+
+        if (shift === "left") {
+            if (pos !== 0) {
+                setPos(pos - 1);
+                setItems(pokemonsList[pos]?.map(pokemon => ({ label: pokemon.name, value: pokemon.id })));
+            }
+        }
+
+        if (shift === "right") {
+            if (pos !== pokemonsList.length - 1) {
+                setPos(pos + 1);
+                setItems(pokemonsList[pos]?.map(pokemon => ({ label: pokemon.name, value: pokemon.id })));
+            }
+        }
+    }
+
+
+    useInput((_input, key) => {
+        if (key.leftArrow) {
+            listShift("left");
+        }
+        if (key.rightArrow) {
+            listShift("right");
+        }
+    })
+
     if (flag.pokemon) {
         return <PokemonUI name={flag.pokemon} />
     }
 
     return <>
-        {Database.getAllPokemon().map(pokemon => <Text key={pokemon.name}>{pokemon.name}</Text>)}
+
+        <SelectInput items={items} onSelect={selectHandler} />
     </>
 }
 
