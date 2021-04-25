0541ba58a15266252cb839ad682f494e3225dbf9
Souvik De <souvikde.ns@gmail.com>
2021-04-25 15:40:01 +0530

    Update search.tsx

--- source/components/pokedex/search.tsx
+++ source/components/pokedex/search.tsx
@@ -1,29 +1,17 @@
 import React, { FC, useState } from 'react';
 import TextInput from 'ink-text-input';
 import { Box, Text } from 'ink'
-import { Database, _Pokemon } from '../../lib';
 import _ from 'lodash';
 
 const Search: FC<{}> = () => {
-    const pokemonList: Array<_Pokemon> = Database.getAllPokemon();
     var [query, setQuery] = useState('');
-    var [searchList, setSearchList] = useState([]);
-
-    const textInputOnChange = (value: any) => {
-        setQuery(value);
-
-        setSearchList(_.filter(pokemonList, pokemon => pokemon.name === "bulbasaur"))
-
-    }
 
     return <>
         <Box>
             <Text>Search: </Text>
 
-            <TextInput value={query} onChange={textInputOnChange} />
+            <TextInput value={query} onChange={setQuery} />
         </Box>
-
-        {searchList.map(pokemon => <Text>{pokemon.name}</Text>)}
     </>
 }
 
